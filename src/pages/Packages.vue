35418fab9731ef96ad412df5f5bf96b9d1d4ab92
Jakrarat Chunnananda <jade.jch@gmail.com>
2019-03-27 19:40:14 -0700

    Packages connect to backend

--- src/pages/Packages.vue
+++ src/pages/Packages.vue
@@ -6,13 +6,16 @@
         class="table"
         title="Packages"
         :columns="packCols"
-        :rows="packages"
+        :rows="packRows"
         :globalSearch="true"
         :paginate="true"
         :perPage="5"
       >
       </vue-good-table>
     </div>
+  <div>
+    <p>Add or Modify</p>
+  </div>
 
   </div>
 </template>
@@ -20,68 +23,62 @@
 <script>
 import { mapState, mapActions } from "vuex";
 import { VueGoodTable } from "vue-good-table";
+import axios from "axios";
 
 export default {
   name: "Packages",
   data() {
     return {
+      packRows: [],
       packCols: [
         {
           label: "Tracking No.",
-          field: "Package_TrackingNo",
+          field: "package_trackingno",
           filterable: true
         },
         {
           label: "Content",
-          field: "Package_Content",
+          field: "package_content",
           filterable: true
         },
         {
           label: "Type",
-          field: "Package_Type",
+          field: "package_type",
           filterable: true
         },
         {
           label: "Status",
-          field: "Package_Status",
+          field: "package_status",
           filterable: true
         },
         {
           label: "Driver",
-          field: "Employee_Name",
+          field: "package_driverid",
           filterable: true
         }
       ]
     };
   },
   computed: {
-    packages: function() {
-      if (
-        this.$store.state.Packages == null ||
-        typeof this.$store.state.Packages.data == "undefined"
-      ) {
-        return [
-          {
-            Package_TrackingNo: "1294YE32",
-            Package_Content: "Documents",
-            Package_Type: "Envelope",
-            Package_Status: "En Route",
-            Employee_Name: "Jarred Huskell"
-          },
-          {
-            Package_TrackingNo: "4252YE30",
-            Package_Content: "Furniture",
-            Package_Type: "Crate",
-            Package_Status: "At shipment facility",
-            Employee_Name: "Kevin Matthew"
-          }
-        ];
-      } else {
-        return this.$store.state.Packages.data;
-      }
+  rows: function () { 
+    return this.packRows;
+  }
+},
+  created: function() {
+    this.next()
+},
+  methods: {
+    next() {
+      console.log("Sending")
+      axios.defaults.headers.common['x-requested-with'] = 'local';
+      axios.get("https://cors-anywhere.herokuapp.com/ec2-54-86-52-215.compute-1.amazonaws.com:3000/packages")
+      .then(response => {
+        console.log(response.data[0]);
+        this.packRows = response.data;
+      }).catch(err => {
+        console.log(err);
+      })
     }
-  },
-  created() {},
-  methods: {}
+},
 };
 </script>
