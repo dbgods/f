554f0b135e4544b74c0c8748ec40026e5bdd5cb4
Sam Parhimchik <d1j0b@ugrad.cs.ubc.ca>
2019-03-28 18:18:57 -0700

    cssssss

--- src/app/App.vue
+++ src/app/App.vue
@@ -4,52 +4,52 @@
       <ul v-show="!isNotLoggedIn" style="width: 100% !important; padding-inline-start: 0px !important;">
         <img src="../assets/logo.png" style="width: 13%; height: 13%;"alt="">
         <li style="display:inline-block;margin-right:5px;">
-          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important; padding: 15px 22px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
+          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important; padding: 15px 10px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
               <router-link to="/" class="router">Home</router-link>
           </button>
         </li>
         <li style="display:inline-block;margin-right:5px;">
-          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 22px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
+          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 10px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
           <router-link to="/Packages" class="router">Packages</router-link>
           </button>
         </li>
         <li style="display:inline-block;margin-right:5px;">
-          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 22px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
+          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 10px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
           <router-link to="/Complaints" class="router">Complaints</router-link>
           </button>
         </li>
         <li style="display:inline-block;margin-right:5px;">
-          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 22px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
+          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 10px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
           <router-link to="/Vehicles" class="router">Vehicles</router-link>
           </button>
         </li>
         <li style="display:inline-block;margin-right:5px;">
-          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 22px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
+          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 10px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
           <router-link to="/Drivers" class="router">Drivers</router-link>
           </button>
         </li>
         <li v-show="isManager" style="display:inline-block;margin-right:5px;" >
-          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 22px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
+          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 10px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
           <router-link to="/Employees" class="router">Employees</router-link>
           </button>
         </li>
         <li v-show="isManager" style="display:inline-block;margin-right:5px;" >
-          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 22px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
+          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 10px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
           <router-link to="/Customers" class="router">Customers</router-link>
           </button>
         </li>
         <li v-show="isManager" style="display:inline-block;margin-right:5px;" >
-          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 22px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
+          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 10px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
           <router-link to="/Custom" class="router">Custom</router-link>
           </button>
         </li>
         <li v-show="isManager" style="display:inline-block;margin-right:10px;" >
-          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 22px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
+          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffe44c !important;padding: 15px 10px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
           <router-link to="/Stats" class="router">Statistics</router-link>
           </button>
         </li>
         <li style="display:inline-block;text-align:right;">
-          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffcc4c !important;padding: 15px 22px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
+          <button type="button" name="button" style=" border-radius: 5px !important;background-color: #ffcc4c !important;padding: 15px 10px !important; text-align: center !important;text-decoration: none !important; display: inline-block !important; font-size: 16px !important;">
           <router-link to="/login"> Logout</router-link>
           </button>
         </li>
